7bec518698268b37e254195de611b64708a43f6b
Felix <xiofelix725@gmail.com>
2024-08-30 18:44:10 +0930

    Update client.py

--- client/client.py
+++ client/client.py
@@ -1,40 +1,33 @@
-import sys
-from connection import ClientConnection
-from encryption import encrypt_message, decrypt_message
+import socket
+from encryption import encrypt_message
+from cryptography.hazmat.primitives import serialization
 
 
 class Client:
     def __init__(self, server_ip, server_port):
-        self.connection = ClientConnection(server_ip, server_port)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((server_ip, server_port))
+        self.server_public_key = self.receive_public_key()
+
+    def receive_public_key(self):
+        # 接收来自服务器的公钥
+        pem_data = self.sock.recv(1024)  # 假设公钥小于1024字节
+        public_key = serialization.load_pem_public_key(pem_data)
+        return public_key
 
     def send_message(self, message):
-        encrypted_message = encrypt_message(message)
-        self.connection.send(encrypted_message)
-
-    def receive_message(self):
-        encrypted_message = self.connection.receive()
-        message = decrypt_message(encrypted_message)
-        return message
+        encrypted_message = encrypt_message(message, self.server_public_key)
+        self.sock.send(encrypted_message)
 
     def start(self):
-        print("Client started. Type 'quit' to exit.")
         while True:
             message = input("Enter message: ")
             if message.lower() == "quit":
+                print("Client disconnected.")
                 break
             self.send_message(message)
-            print("Message sent.")
-            response = self.receive_message()
-            print("Received:", response)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python client.py <server_ip> <server_port>")
-        sys.exit(1)
-
-    server_ip = sys.argv[1]
-    server_port = int(sys.argv[2])
-
-    client = Client(server_ip, server_port)
+    client = Client("127.0.0.1", 8080)  # 假设服务器运行在本地并监听8080端口
     client.start()
