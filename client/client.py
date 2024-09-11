051f24e3a7bad4d6d86040653595c5a5a015bc08
Felix <xiofelix725@gmail.com>
2024-09-12 01:51:18 +0930

    Implementing signature check

--- client/client.py
+++ client/client.py
@@ -1,20 +1,37 @@
 import socket
+import os
 from protocol.message_format import create_message
-from shared.encryption import encrypt_message
+from shared.encryption import encrypt_message, sign_message
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
+
 
 class Client:
     def __init__(self, server_ip, server_port):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((server_ip, server_port))
-        self.private_key = rsa.generate_private_key(
-            public_exponent=65537,
-            key_size=2048
-        )
+        self.private_key = self.load_or_generate_private_key()
         self.server_public_key = self.receive_public_key()
         self.counter = 0
         self.send_hello()
+
+    # Load the private key from a file or generate a new one
+    def load_or_generate_private_key(self):
+        if os.path.exists("client_private_key.pem"):
+            with open("client_private_key.pem", "rb") as key_file:
+                private_key = serialization.load_pem_private_key(
+                    key_file.read(), password=None
+                )
+        else:
+            private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+            with open("client_private_key.pem", "wb") as key_file:
+                pem = private_key.private_bytes(
+                    encoding=serialization.Encoding.PEM,
+                    format=serialization.PrivateFormat.TraditionalOpenSSL,
+                    encryption_algorithm=serialization.NoEncryption(),
+                )
+                key_file.write(pem)
+        return private_key
 
     def receive_public_key(self):
         pem_data = self.sock.recv(1024)
@@ -29,8 +46,14 @@ class Client:
 
     def send_message(self, message_type, data):
         self.counter += 1
-        json_message = create_message(message_type, data, self.private_key, self.counter)
-        encrypted_message = encrypt_message(json_message, self.server_public_key)
+        # Create the message and sign it
+        json_message = create_message(
+            message_type, data, self.private_key, self.counter
+        )
+        signature = sign_message(self.private_key, json_message)
+        encrypted_message = encrypt_message(
+            json_message, self.server_public_key, signature
+        )
         self.sock.send(encrypted_message)
 
     def start(self):
@@ -46,6 +69,7 @@ class Client:
                 break
 
         self.sock.close()
+
 
 if __name__ == "__main__":
     client = Client("127.0.0.1", 8080)
