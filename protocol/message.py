e9351dceb5db11cce2a7baef0f1962cb46e0fe41
Felix <xiofelix725@gmail.com>
2024-09-23 00:01:16 +0930

    Remove unnecessary files

--- protocol/message.py
+++
@@ -1,9 +0,0 @@
-from olaf_protocol import OLAFProtocol
-
-
-def create_message(sender, receiver, message):
-    return OLAFProtocol.format_message(message, sender, receiver)
-
-
-def parse_message(formatted_message):
-    return OLAFProtocol.parse_message(formatted_message)
