92770fed0efb2fc55d408bacaad1481cf2e1692c
Felix <xiofelix725@gmail.com>
2024-09-24 12:33:21 +0930

    Restart

--- scripts/run_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Add project root to PYTHONPATH to make sure Python can find the modules
-export PYTHONPATH=$(pwd)
-python3 ./server/server.py --host 127.0.0.1 --port 8080 --server_port 8081
-python3 ./server/server.py --host 127.0.0.1 --port 8082 --server_port 8083
